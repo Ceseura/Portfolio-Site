a63da4e511d3610a4a1a73069a7480b839e1b341
Alex <unpiraticalturtle@gmail.com>
2018-09-07 20:58:18 -0400

    deleted some unnecessary files

--- src/components/PortfolioCard.vue
+++ src/components/PortfolioCard.vue
@@ -19,7 +19,7 @@ import Hexagon from '@/components/PortfolioCardHexagon.vue';
 import data from '@/assets/projects.json';
 
 var a = [1, -1, 6, 0,
-         -1, 2, 4, 8,
+         -1, 2, 4, 0,
          -1, 3, 0, 7,
          0, 5, 0];
 
